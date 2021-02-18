9edf795066e67482ed20402b1e6be1e3ad082b05
IndraneelBabharey <76774063+IndraneelBabharey@users.noreply.github.com>
2021-02-18 16:06:07 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,36 @@
+class paper{
+    constructor(x,y){
+var options = {
+     isStatic : false,
+    restitution : 0.3,
+    friction : 0.5,
+    density : 1.2
+   
+}
+
+  this.body = Bodies.circle(x, y, 20, options);
+  this.radius = 20
+  this.image = loadImage("paper.png")
+
+  World.add(world, this.body);
+    }
+
+    display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        //ellipseMode(RADIUS);
+        imageMode(CENTER)
+       
+        //circle(0, 0, this.radius);
+        image(this.image,0,0,this.radius*2,this.radius*2)
+        pop();
+      }
+
+
+
+
+
+    }
